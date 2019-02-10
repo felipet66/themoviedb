1fe41e98023c6dd3280547a2a8f3fbb01c461f8d
Felipe Teles <fliptm95@gmail.com>
2019-02-10 17:51:41 -0200

    Component Head, Home e Footer

--- the-movie-db/src/app/app.module.ts
+++ the-movie-db/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
